d761a37ec1b173bac898f624bd9b54dcb895600f
Domenico Gemoli <domenico.gemoli@booking.com>
2020-09-08 15:45:37 +0200

    Use atom feed for more reliable data

--- src/content.js
+++ src/content.js
@@ -1,23 +1,46 @@
 (() => {
 	let unreadCount;
 
-	function getUnreadCount() {
-		const inboxNavigationElement = document.querySelector('div[role=navigation] [aria-label*="Inbox"]');
-		if (!inboxNavigationElement) {
-			return 0;
+	function getUnreadCount(doc) {
+		if (!doc) {
+			return -1;
 		}
-		const countText = inboxNavigationElement.getAttribute('aria-label').replace(/[^0-9]/g, '');
-		const count = parseInt(countText);
-		return isNaN(count) ? 0 : count;
+		const fullcountElement = doc.querySelector('fullcount');
+		if (!fullcountElement) {
+			return -1;
+		}
+		const count = parseInt(fullcountElement.textContent);
+		if (isNaN(count)) {
+			return -1;
+		}
+		return count;
 	}
 
-	function updateBadgeIcon() {
-		const newUnreadCount = getUnreadCount();
+	function getAtomFeed() {
+		return new Promise((resolve) => {
+			const x = new XMLHttpRequest();
+			x.open('GET', 'https://mail.google.com/mail/feed/atom?_=' + new Date().getTime(), true);
+			x.setRequestHeader('Cache-Control', 'no-cache');
+			x.onreadystatechange = function () {
+				if (x.readyState == 4 && x.status == 200) {
+					resolve(x.responseXML);
+				}
+			};
+			x.send(null);
+		});
+	}
+
+	async function updateBadgeIcon() {
+		const feed = await getAtomFeed();
+		const newUnreadCount = getUnreadCount(feed);
+		if (newUnreadCount < 0) {
+			return;
+		}
 		if (newUnreadCount !== unreadCount) {
 			unreadCount = newUnreadCount
 			navigator.setAppBadge(unreadCount);
 		}
 	}
 
-	setInterval(updateBadgeIcon, 500);
+	setInterval(updateBadgeIcon, 1000);
 })();
