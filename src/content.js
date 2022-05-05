7442d1a2d3b68e7043e5213ecb016480a15de0de
Jason Miller <developit@users.noreply.github.com>
2022-05-05 12:26:23 -0400

    use document title to avoid network spam

--- src/content.js
+++ src/content.js
@@ -1,46 +1,6 @@
-(() => {
-	let unreadCount;
-
-	function getUnreadCount(doc) {
-		if (!doc) {
-			return -1;
-		}
-		const fullcountElement = doc.querySelector('fullcount');
-		if (!fullcountElement) {
-			return -1;
-		}
-		const count = parseInt(fullcountElement.textContent);
-		if (isNaN(count)) {
-			return -1;
-		}
-		return count;
-	}
-
-	function getAtomFeed() {
-		return new Promise((resolve) => {
-			const x = new XMLHttpRequest();
-			x.open('GET', 'https://mail.google.com/mail/feed/atom?_=' + new Date().getTime(), true);
-			x.setRequestHeader('Cache-Control', 'no-cache');
-			x.onreadystatechange = function () {
-				if (x.readyState == 4 && x.status == 200) {
-					resolve(x.responseXML);
-				}
-			};
-			x.send(null);
-		});
-	}
-
-	async function updateBadgeIcon() {
-		const feed = await getAtomFeed();
-		const newUnreadCount = getUnreadCount(feed);
-		if (newUnreadCount < 0) {
-			return;
-		}
-		if (newUnreadCount !== unreadCount) {
-			unreadCount = newUnreadCount
-			navigator.setAppBadge(unreadCount);
-		}
-	}
-
-	setInterval(updateBadgeIcon, 1000);
-})();
+Object.defineProperty(document, 'title', {
+  configurable: true,
+  set(v) {
+    navigator.setAppBadge((String(v).match(/\((\d+)\)/) || [])[1] | 0 || null);
+  }
+});
