040a3a81057c643a0c854d9c9314d099e50a0f8a
Jason Miller <developit@users.noreply.github.com>
2022-05-05 12:33:39 -0400

    Update content.js

--- src/content.js
+++ src/content.js
@@ -1,6 +1,12 @@
-Object.defineProperty(document, 'title', {
-  configurable: true,
-  set(v) {
-    navigator.setAppBadge((String(v).match(/\((\d+)\)/) || [])[1] | 0 || null);
-  }
-});
+(() => {
+  let desc = Object.getOwnPropertyDescriptor(Document.prototype, 'title');
+  Object.defineProperty(document, 'title', {
+    configurable: true,
+    set(v) {
+      let m = String(v).match(/Inbox(?: \((\d+)\)) -/);
+      if (m) navigator.setAppBadge((m[1]|0) || null);
+      desc.set.call(this, v);
+    },
+    get() { return desc.get.call(this); }
+  });
+})();
